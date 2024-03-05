d4c75b7568f771e95e0f127a8aa46cb5e29909c2
João Marcelo <joaomarcelo.co@hotmail.com>
2024-03-05 08:59:12 -0300

    add: class 01 module 03 TAJS concepts and setup mocks

---
+++ modulo03-conceitos-tajs/aula-01-setup-mocks/README.md
@@ -0,0 +1,25 @@
+Testes unitários devem ser 100% offline e não deve ter qualquer iteração externa
+
+- Receber o CPF e o nome do cliente
+- Validar se o CPF é válido
+- Validar que o nome do cliente possui no máximo 100 caracteres
+- formatar os dados do cliente
+- salvar no banco de dados
+
+a. receber pessoa
+b. validar dados
+c. formatar dados
+d. registrar
+
+recuperar dependências
+- npm ci --silent
+
+
+mock é a entrada necessária para que o teste funcione
+
+teste unitário é "mock x objeto"
+
+3 A's dos testes
+- Arrange -> preparar
+- Act -> executar
+- Assert -> validar
---
+++ modulo03-conceitos-tajs/aula-01-setup-mocks/test/person.test.js
@@ -0,0 +1,105 @@
+import { describe, it, expect, jest } from "@jest/globals";
+import Person from "../src/Person";
+
+describe('#Person Suite', () => {
+    describe('#validate', () => {
+        it('should throw an error when there is no name', () => {
+            const mockInvalidName = {
+                name: '',
+                cpf: '123.456.789-00'
+            }
+
+            expect(() => Person.validate(mockInvalidName))
+                .toThrow(new Error('Name is required'))
+        })
+        it('should throw an error when there is no cpf', () => {
+            const mockInvalidCPF = {
+                name: 'John Doe',
+                cpf: ''
+            }
+
+            expect(() => Person.validate(mockInvalidCPF))
+                .toThrow(new Error('CPF is required'))
+        })
+        it('should not throw an error when there is name and cpf', () => {
+            const mockValidPerson = {
+                name: 'John Doe',
+                cpf: '123.456.789-00'
+            }
+
+            expect(() => Person.validate(mockValidPerson))
+                .not
+                .toThrow()
+        })
+    })
+
+    describe('#format', () => {
+        it('should format the person name and CPF', () => {
+            // parte do principio que os dados já foram validados
+            // Arrange
+            const mockPerson = {
+                name: 'Xuxa da silva',
+                cpf: '123.456.789-00',
+            }
+
+            // Act
+            const formattedPerson = Person.format(mockPerson)
+
+            // Assert
+            const expected = {
+                name: 'Xuxa',
+                cpf: '12345678900',
+                lastName: 'da silva'
+            }
+
+            expect(formattedPerson).toStrictEqual(expected)
+        })
+    })
+
+    describe('#process', () => {
+        it('should process a valid person', () => {
+            //    Uma outra ideia é não retestr o que já foi testado
+            //    Lembra dos checkpoints?
+            //    Testou do caminho A para o caminho B, agora testa do caminho B ao C
+            //    Então aqui, eu pulo o caminho A (validate), caminho B (format), pois estes caminhos já foram validados e vou direto para o caminho C (save)
+            //    Este método abaixo faz mais sentido para quando se tem interaçoes externas como chamadas de API, Bancos de dados e etc que serão mostrados na próxima aula
+            //    Mocks são simulações de funções que você pode fazer ao testar o comportamento
+
+            // AAA: Arrange, Act, Assert
+
+            // Arrange
+            const mockPerson = {
+                name: 'Xuxa da silva',
+                cpf: '123.456.789-00',
+            }
+            jest
+                .spyOn(
+                    Person,
+                    Person.validate.name
+                )
+                .mockReturnValue()
+                // .mockImplementation(() => {
+                //     throw new Error('Deu ruim!!')
+                // })
+
+            jest
+                .spyOn(
+                    Person,
+                    Person.format.name
+                )
+                .mockReturnValue({
+                    cpf: '12345678900',
+                    name: 'Xuxa',
+                    lastName: 'da Silva'
+                })
+
+            // Act
+            const result = Person.process(mockPerson)
+
+            // Assert
+            const expected = 'ok'
+            expect(result).toStrictEqual(expected)
+
+        })
+    })
+})
