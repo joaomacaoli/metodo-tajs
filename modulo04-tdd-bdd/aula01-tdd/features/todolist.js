b3d9e552c46c44c124edbfa3edc727a826107e5a
João Marcelo <joaomarcelo.co@hotmail.com>
2024-04-10 20:57:44 -0300

    change directory name

---
+++ modulo04-tdd-bdd/aula01-tdd/features/todolist.js
@@ -0,0 +1,11 @@
+/**
+ * Deve cadastrar usuários e definir uma categoria onde:
+ * - Jovens adultos
+ *    - Usuarios de 18-25 anos
+ * - Adultos
+ *    - Usuarios de 26-50 anos
+ * - Jovens adultos
+ *    - Usuarios de 51+
+ * - Menor
+ *    - Estoura um erro
+ */
